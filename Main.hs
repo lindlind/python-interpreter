2bd9574ec61bbf3ef2a3acd68011456428600415
Vladimir Lind <lindvv@yandex.ru>
2020-10-15 01:11:41 +0300

    extend prettyPrinter with bool and bitwise operations

--- Main.hs
+++ Main.hs
@@ -13,15 +13,33 @@ castTS :: ToS a -> ToS b
 castTS (ToS s) = ToS s
 
 instance IExpr ToS where
+  iAssign a b = (castTS $ (ToS . show) a) <> (ToS " = ") <> castTS b
+  iProcedure = castTS
+  iOr  a b = a <> (ToS " or ")  <> b
+  iAnd a b = a <> (ToS " and ") <> b
+  iNot a = (ToS " not ") <> a
+  iBoolEq  a b = a <> (ToS " == ") <> b
+  iBoolNEq a b = a <> (ToS " != ") <> b
+  iEq  a b = castTS a <> (ToS " == ") <> castTS b
+  iNEq a b = castTS a <> (ToS " != ") <> castTS b
+  iLT  a b = castTS a <> (ToS " < ")  <> castTS b
+  iGT  a b = castTS a <> (ToS " > ")  <> castTS b
+  iLTE a b = castTS a <> (ToS " <= ") <> castTS b
+  iGTE a b = castTS a <> (ToS " >= ") <> castTS b
+  iBitOr  a b = a <> (ToS " | ") <> b
+  iBitXor a b = a <> (ToS " ^ ") <> b
+  iBitAnd a b = a <> (ToS " & ") <> b
+  iLeftShift  a b = a <> (ToS " << ") <> b
+  iRightShift a b = a <> (ToS " >> ") <> b
   iPlus a b = a <> (ToS " + ") <> b
   iMinus a b = a <> (ToS " - ") <> b
   iMult a b = a <> (ToS " * ") <> b
-  iFloatDiv a b = a <> (ToS " / ") <> b
+  iFloatDiv a b = castTS $ a <> (ToS " / ") <> b
   iDiv a b = a <> (ToS " // ") <> b
   iMod a b = a <> (ToS " % ") <> b
-  iVal = ToS . show
-  iAssign a b = (castTS $ (ToS . show) a) <> (ToS " = ") <> castTS b
-  iProcedure = castTS
+  iBoolVal  = ToS . show
+  iIntVal   = ToS . show
+  iFloatVal = ToS . show
 
 prettyPrint :: Either String [Statement] -> String
 prettyPrint (Left s) = s
